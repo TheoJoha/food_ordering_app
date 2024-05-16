43fda05bcc58dce275c04332aa82f2bc3ea6c5e4
Theodor Johansson <t.j.u@telia.com>
2024-05-16 19:41:21 +0200

    mobile hamburger menu and nav

--- frontend/src/layouts/layouts.tsx
+++ frontend/src/layouts/layouts.tsx
@@ -6,7 +6,7 @@ type Props = {
 
 const Layout = ({children}: Props) => {
     return (
-        <div className="flex flex.col min-h-screen">
+        <div className="flex flex-col min-h-screen">
             <Header />
 
             <div className="container mx-auto flex-1 py-10">
