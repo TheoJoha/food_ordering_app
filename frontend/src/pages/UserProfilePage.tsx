e0c2fef589e8554d63e01b445a8a0b3a33b3e7fc
Theodor Johansson <t.j.u@telia.com>
2024-05-18 15:44:54 +0200

    controller validation routes

---
+++ frontend/src/pages/UserProfilePage.tsx
@@ -0,0 +1,9 @@
+import UserProfielForm from "@/forms/user-profile-form/UserProfielForm"
+
+const UserProfilePage = () => {
+  return (
+    <UserProfielForm />
+  )
+}
+
+export default UserProfilePage
