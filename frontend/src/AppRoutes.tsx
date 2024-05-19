a6527580eaa3f7a3eb457593c14f43dc51152e71
Theodor Johansson <t.j.u@telia.com>
2024-05-19 17:10:17 +0200

    protected route

--- frontend/src/AppRoutes.tsx
+++ frontend/src/AppRoutes.tsx
@@ -3,6 +3,7 @@ import Layout from "./layouts/layouts"
 import HomePage from "./pages/HomePage"
 import AuthCallbackPage from "./pages/AuthCallbackPage"
 import UserProfilePage from "./pages/UserProfilePage"
+import ProtectedRoute from "./auth/ProtectedRoute"
 
 const AppRoutes = () => {
     return (
@@ -10,10 +11,13 @@ const AppRoutes = () => {
             <Route path="/"
                 element={<Layout showHero><HomePage /></Layout>} />
             <Route path="/auth-callback" element={<AuthCallbackPage />} />
-            <Route path="/user-profile"
-                element={<Layout>
-                    <UserProfilePage />
-                </Layout>} />
+            <Route element={<ProtectedRoute />}>
+                <Route path="/user-profile"
+                    element={<Layout>
+                        <UserProfilePage />
+                    </Layout>} />
+            </Route>
+
             <Route path="*" element={<Navigate to="/" />} />
         </Routes>
     )
