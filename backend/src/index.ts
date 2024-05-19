bdeb0bd21fd5cee00dcf8b6dc2082ea51d065a54
Theodor Johansson <t.j.u@telia.com>
2024-05-19 17:57:38 +0200

    backend prod dist build

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -10,6 +10,10 @@ const app = express();
 app.use(express.json());
 app.use(cors())
 
+app.get("/health", async (req: Request, res: Response) => {
+    res.send({message: "health ok!"})
+})
+
 app.use("/api/my/user", myUserRoute);
 
 app.listen(3003, () => {
