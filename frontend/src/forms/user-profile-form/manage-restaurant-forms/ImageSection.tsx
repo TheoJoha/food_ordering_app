1035ffcaeaa75cbb6d21df1de07b3d6f005e2f0d
Theodor Johansson <t.j.u@telia.com>
2024-05-20 19:04:08 +0200

    manage restaurant

--- frontend/src/forms/user-profile-form/manage-restaurant-forms/ImageSection.tsx
+++ frontend/src/forms/user-profile-form/manage-restaurant-forms/ImageSection.tsx
@@ -1,9 +1,12 @@
+import { AspectRatio } from "@/components/ui/aspect-ratio"
 import { FormControl, FormDescription, FormField, FormItem, FormMessage } from "@/components/ui/form"
 import { Input } from "@/components/ui/input"
 import { useFormContext } from "react-hook-form"
 
 const ImageSection = () => {
-    const { control } = useFormContext()
+    const { control, watch } = useFormContext()
+
+    const existingImageUrl = watch("imageUrl")
 
     return (
         <div className="space-y-2">
@@ -15,7 +18,12 @@ const ImageSection = () => {
                     Add an image that will be displayed on your restaurant listings in the search results. Adding a new image will overwrite an existing one.
                 </FormDescription>
             </div>
-            <div className="flex flex-col gap-8 2-[50%]">
+            <div className="flex flex-col gap-8 md:w-[50%]">
+                {existingImageUrl && (
+                    <AspectRatio ratio={16/9} >
+                        <img alt="img" src={existingImageUrl} className="rounded-md object-cover h-full w-full" />
+                    </AspectRatio>
+                )}
                 <FormField control={control} name="imageFile" render={({ field }) => <FormItem>
                     <FormControl>
                         <Input className="bg-white" type="file" accept=".jpg, .jpeg, .png" 
--- frontend/src/pages/ManageRestaurantPage.tsx
+++ frontend/src/pages/ManageRestaurantPage.tsx
@@ -1,10 +1,11 @@
-import { useCreateMyRestaurant } from "@/api/MyRestaurantApi"
+import { useCreateMyRestaurant, useGetMyRestaurant } from "@/api/MyRestaurantApi"
 import ManageRestaurantForm from "@/forms/user-profile-form/manage-restaurant-forms/ManageRestaurantForm"
 
 const ManageRestaurantPage = () => {
     const {createRestaurant, isLoading} = useCreateMyRestaurant()
+    const {restaurant} = useGetMyRestaurant()
   return (
-   <ManageRestaurantForm onSave={createRestaurant} isLoading={isLoading} />
+   <ManageRestaurantForm restaurant={restaurant} onSave={createRestaurant} isLoading={isLoading} />
   )
 }
 
