393a0f56a28533e3c010bc33ba1d3039b0cdc09e
Theodor Johansson <t.j.u@telia.com>
2024-05-21 22:19:50 +0200

    updated search functionality

--- frontend/src/components/SearchBar.tsx
+++ frontend/src/components/SearchBar.tsx
@@ -5,6 +5,7 @@ import { Form, FormControl, FormField, FormItem } from "./ui/form";
 import { Search } from "lucide-react";
 import { Input } from "./ui/input";
 import { Button } from "./ui/button";
+import { useEffect } from "react";
 
 const formSchema = z.object({
     searchQuery: z.string({
@@ -18,16 +19,24 @@ type Props = {
     onSubmit: (formData: SearchForm) => void;
     placeHolder: string;
     onReset?: () => void;
+    searchQuery: string;
 }
 
-const SearchBar = ({ onSubmit, onReset, placeHolder }: Props) => {
+const SearchBar = ({ onSubmit, onReset, placeHolder, searchQuery }: Props) => {
     const form = useForm<SearchForm>({
         resolver: zodResolver(formSchema),
+        defaultValues: {
+            searchQuery,
+        },
     })
+
+    useEffect(() => {
+        form.reset({searchQuery})
+    },[form, searchQuery])
 
     const handleReset = () => {
         form.reset({
-                searchQuery: "",
+            searchQuery: "",
         })
 
         if (onReset) {
@@ -37,7 +46,7 @@ const SearchBar = ({ onSubmit, onReset, placeHolder }: Props) => {
 
     return (
         <Form {...form}>
-            <form onSubmit={form.handleSubmit(onSubmit)} className={`flex items-center flex-1 gap-3 justify-between flex-row border-2 rounded-full p-3 mx-5 ${form.formState.errors.searchQuery && "border-red-500"}`} >
+            <form onSubmit={form.handleSubmit(onSubmit)} className={`flex items-center flex-1 gap-3 justify-between flex-row border-2 rounded-full p-3 ${form.formState.errors.searchQuery && "border-red-500"}`} >
                 <Search strokeWidth={2.5} size={30} className="ml-1 text-orange-500 hidden md:block" />
                 <FormField control={form.control} name="searchQuery" render={({ field }) => (<FormItem className="flex-1" >
                     <FormControl>
@@ -48,11 +57,9 @@ const SearchBar = ({ onSubmit, onReset, placeHolder }: Props) => {
                 </FormItem>
                 )}
                 />
-                    {form.formState.isDirty && (
                     <Button onClick={handleReset} type="button" variant="outline" className="rounded-full" >
-                        Clear
+                        Reset
                     </Button>
-                    )}
                     <Button type="submit" className="rounded-full bg-orange-500">Search</Button>
             </form>
         </Form>
--- frontend/src/pages/SearchPage.tsx
+++ frontend/src/pages/SearchPage.tsx
@@ -1,11 +1,33 @@
 import { useSearchRestaurants } from "@/api/RestaurantApi"
+import SearchBar, { SearchForm } from "@/components/SearchBar"
 import SearchResultsCard from "@/components/SearchResultsCard"
 import { SearchResultsInfo } from "@/components/SearchResultsInfo"
+import { useState } from "react"
 import { useParams } from "react-router-dom"
+
+export type SearchState = {
+    searchQuery: string;
+}
 
 const SearchPage = () => {
     const { city } = useParams()
-    const { results, isLoading } = useSearchRestaurants(city)
+    const [searchState, setSearchState] = useState<SearchState>({
+        searchQuery: "",
+    })
+    const { results, isLoading } = useSearchRestaurants(searchState, city)
+
+    const setSearchQuery = (searchFormData: SearchForm) => {
+        setSearchState((prevState) => ({
+            ...prevState, searchQuery: searchFormData.searchQuery,
+        }))
+    }
+
+    const resetSearch = () => {
+        setSearchState((prevState) => ({
+            ...prevState,
+            searchQuery: "",
+        }))
+    }
 
     if (isLoading) {
         <span>Loading...</span>
@@ -21,9 +43,17 @@ const SearchPage = () => {
                 insert cuisines here:
             </div>
             <div id="main-content" className="flex flex-col gap-5">
-                <SearchResultsInfo total={results.pagination.total} city={city} />
+                <SearchBar
+                    searchQuery={searchState.searchQuery}
+                    onSubmit={setSearchQuery} placeHolder="Search by cuisine or restaurant name"
+                    onReset={resetSearch}
+                />
+                <SearchResultsInfo
+                    total={results.pagination.total} city={city}
+                />
                 {results.data.map((restaurant) => (
-                    <SearchResultsCard restaurant={restaurant} />
+                    <SearchResultsCard restaurant={restaurant}
+                    />
                 ))}
             </div>
         </div>
