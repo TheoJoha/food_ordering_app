2815454e3c7b8590124da1ddb5dbdf7ca9aa8662
Theodor Johansson <t.j.u@telia.com>
2024-05-24 16:16:30 +0200

    stripe webhook

--- backend/src/controllers/OrderController.ts
+++ backend/src/controllers/OrderController.ts
@@ -21,6 +21,13 @@ type CheckoutSessionRequest = {
     restaurantId: string;
 }
 
+const stripeWebhookHandler = async (req: Request, res: Response) => {
+    console.log("RECEIVED EVENT")
+    console.log("==============")
+    console.log("event: ", req.body)
+    res.send()
+}
+
 const createCheckoutSession = async (req: Request, res: Response) => {
     try {
         const checkoutSessionRequest: CheckoutSessionRequest = req.body;
@@ -116,5 +123,6 @@ const createSession = async (lineItems: Stripe.Checkout.SessionCreateParams.Line
 }
 
 export default {
-    createCheckoutSession
+    createCheckoutSession,
+    stripeWebhookHandler
 }
